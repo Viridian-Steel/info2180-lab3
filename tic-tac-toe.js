67ad5fda673b3e5bb0fd474b473cfd0c1b5ca571
Viridian Steel <56614399+Viridian-Steel@users.noreply.github.com>
2023-10-23 00:08:26 -0500

    added onClick event to tiles

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,16 +1,42 @@
+let gameState = [0,0,0,0,0,0,0,0,0];
 
+
+let isX = true;
+const divs = document.getElementsByTagName("div");
 
 function InitSquares() {
-    const divs = document.getElementsByTagName("div");
     console.log("test");
     if(divs == null) {console.error("BIIIG PROBLEM");}
 
-    for( var box of divs){
-
+    let pos = 0;
+    for(let i = 0; i < divs.length; i++){
+        let box = divs[i];
         if(box.id == "" && box.className != "controls"){
+
             box.className = "square";
+            box.addEventListener("click", () =>{
+                if(isX){
+                    box.className = "square X";
+                    box.innerHTML = "X";
+                    gameState[pos] = 1;
+                    isX = false;
+                }
+                else {
+                    box.className = "square O";
+                    box.innerHTML = "O";
+                    gameState[pos] = -1;
+                    isX = true;
+                }
+            });
+            pos++;
+            console.log("set box");
         }
     }
 }
 
+
+
 window.onload = InitSquares;
+
+
+
